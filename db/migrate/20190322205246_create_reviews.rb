744256e38d0fc672d731337bba9fb7d438b4e63f
Rachel Gould <rachegould@gmail.com>
2019-03-22 21:32:41 +0000

    add foreign keys

--- db/migrate/20190322205246_create_reviews.rb
+++ db/migrate/20190322205246_create_reviews.rb
@@ -1,10 +1,12 @@
 class CreateReviews < ActiveRecord::Migration
   def change
     create_table :reviews do |t|
-      t.integer :product_id
-      t.integer :user_id
+      t.references :product, index: true, foreign_key: true
+      t.references :user, index: true, foreign_key: true
       t.string :description
       t.integer :rating
+      t.datetime "created_at", null: false
+      t.datetime "updated_at", null: false
 
       t.timestamps null: false
     end
--- db/schema.rb
+++ db/schema.rb
@@ -65,6 +65,9 @@ ActiveRecord::Schema.define(version: 20190322205246) do
     t.datetime "updated_at",  null: false
   end
 
+  add_index "reviews", ["product_id"], name: "index_reviews_on_product_id", using: :btree
+  add_index "reviews", ["user_id"], name: "index_reviews_on_user_id", using: :btree
+
   create_table "users", force: :cascade do |t|
     t.string   "name"
     t.string   "email"
@@ -76,4 +79,6 @@ ActiveRecord::Schema.define(version: 20190322205246) do
   add_foreign_key "line_items", "orders"
   add_foreign_key "line_items", "products"
   add_foreign_key "products", "categories"
+  add_foreign_key "reviews", "products"
+  add_foreign_key "reviews", "users"
 end
