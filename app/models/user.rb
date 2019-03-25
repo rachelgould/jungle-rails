3bb56d61c3e889e66728cb9acb6d28408b8ce5a0
Rachel Gould <rachegould@gmail.com>
2019-03-25 23:41:57 +0000

    Add edge case support: whitespace and case insensitivity of email addresses

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,8 +4,13 @@ class User < ActiveRecord::Base
   validates :password, presence: true
   validates :name, presence: true
   validates_length_of :password, :minimum => 8
+  before_save :downcase_fields
   
   def self.authenticate_with_credentials(email, password)
-    User.find_by(email: email).try(:authenticate, password) || nil
+    User.find_by(email: email.strip.downcase).try(:authenticate, password) || nil
+  end
+
+  def downcase_fields
+    self.email.downcase!
   end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -38,13 +38,25 @@ RSpec.describe User, type: :model do
   describe '.authenticate_with_credentials' do
     it "authenticate method returns nil for user that doesn't exist" do 
       @result = User.authenticate_with_credentials("test@testers.com", "somepassword")
-      expect(@result).to eql nil
+      expect(@result).to eq nil
     end
 
-    it "authenticate method returns user instance for that can authenticate" do 
+    it "authenticate method returns user instance for if it can authenticate" do 
       @user = User.create(email: "test@testers.com", name: "Rachel", password: "somepassword")
       @result = User.authenticate_with_credentials("test@testers.com", "somepassword")
-      expect(@result).to eql @user
+      expect(@result).to eq @user
+    end
+
+    it "authenticate method returns user instance even if there's extra whitespace around the email" do 
+      @user = User.create(email: "test@testers.com", name: "Rachel", password: "somepassword")
+      @result = User.authenticate_with_credentials("  test@testers.com ", "somepassword")
+      expect(@result).to eq @user
+    end
+
+    it "authenticate method returns user instance regardless of email case" do 
+      @user = User.create(email: "tEst@tESTers.com", name: "Rachel", password: "somepassword")
+      @result = User.authenticate_with_credentials("TesT@testerS.COM", "somepassword")
+      expect(@result).to eq @user
     end
 
   end
