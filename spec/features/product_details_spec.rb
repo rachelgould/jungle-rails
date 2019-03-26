e09836aaf9560acb46e71f4d5a81f1454d009db7
Rachel Gould <rachegould@gmail.com>
2019-03-26 21:36:13 +0000

    Add second way to get to product#show in test

--- spec/features/product_details_spec.rb
+++ spec/features/product_details_spec.rb
@@ -26,6 +26,18 @@ RSpec.feature "Visitor navigates to product page", type: :feature, js: true do
     save_screenshot
 
     # VERIFY
-    expect(page).to have_text Product.first.name
+    expect(page).to have_text(Product.first.name).and have_text "User Reviews:"
+  end
+
+  scenario "They can click the product's image or name to get to that product's details" do
+    # ACT
+    visit root_path
+    find_link(Product.first.name, href: product_path(Product.first.id)).click
+
+    # DEBUG
+    save_screenshot
+
+    # VERIFY
+    expect(page).to have_text(Product.first.name).and have_text "User Reviews:"
   end
 end
